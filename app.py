d6058cd73f3ebb578f26b8cb2ee2136e894f1e9c
maximszeto <otezium@gmail.com>
2026-08-05 19:53:49 -0400

    feat: implement home route and add HTML template for high jump tracker

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! First time trying flask this is kinda cool..!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
