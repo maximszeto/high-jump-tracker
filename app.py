b5c3580eeda5a6a15a1456db1a9fb346efa422e1
maximszeto <otezium@gmail.com>
2026-08-17 08:19:18 -0400

    add guard rails for post request in home

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
 from flask import Flask, render_template, request
-from logic import calculations
+from logic import calculations, database
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        
-        jump_feet = float(request.form.get("feet"))
-        jump_inches = float(request.form.get("inches"))
-        meters = calculations.convertFtToM(jump_feet, jump_inches)
-        print(meters)
+        try:
+            jump_feet = float(request.form.get("feet"))
+            jump_inches = float(request.form.get("inches"))
+            meters = calculations.convertFtToM(jump_feet, jump_inches)
+            print(meters)
 
-        return render_template("index.html", meters=meters)
-    
+            return render_template("index.html", meters=meters)
+
+        except ValueError:
+            return render_template("index.html")
+            
     return render_template("index.html")
     
 @app.route("/stats", methods=["GET", "POST"])
 def stats():    
     if request.method == "POST":
         newJump = float(request.form.get("jump-height"))
-        
+        calculations.addNewLog(newJump, database.highJumpLog) 
 
     return render_template("stats.html")
 
