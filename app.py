7e618ebdc47a5aa1bf1b9e5ba98760709806f446
maximszeto <otezium@gmail.com>
2026-08-25 17:44:14 -0400

    redo address path

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from logic import calculations, database
 
 app = Flask(__name__)
 
-@app.route("/home", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         try:
